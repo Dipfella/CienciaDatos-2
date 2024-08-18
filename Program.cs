ecbc23ea5b4c0dae74abba7cbddec00ab125df18
Julian Sarmiento <julianbranch10@gmail.com>
2024-08-18 11:53:06 -0500

    codigo taller

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Taller_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random _random = new Random();
+            int cantTest = 40;
+            double[] tiempos = new double[cantTest];
+            int[] aCanNum = new int[cantTest];
+
+
+            for (int test = 0; test < cantTest; test++)
+            {
+                int cantNum = _random.Next(100, 1000);
+                Console.WriteLine("\n//-----------------------------------------------------//\n");
+                Console.WriteLine("Cantidad de numeros: " + cantNum);
+                Console.WriteLine("Test #" + (test + 1) + ": \n");
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                int[] randomNum = Utility.GenerateRandomNumbers(cantNum);
+                Utility.PrintArray(randomNum, "Números Array: ");
+                Console.WriteLine();
+                int[] topNum = Utility.GetTopNumbers(randomNum, cantNum);
+                stopwatch.Stop();
+                Utility.PrintArray(topNum, "Top números Array: ");
+                tiempos[test] = stopwatch.Elapsed.TotalMilliseconds;
+                aCanNum[test] = cantNum;
+            }
+            Utility.ExportToCSV("results.csv", tiempos, cantTest, aCanNum);
+        }
+    }
+}
+    
---
+++ Utility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Taller_1
+{
+    public static class Utility
+    {
+        private static readonly Random _random = new Random();
+
+        public static int[] GenerateRandomNumbers(int size)
+        {
+            int[] numbers = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = _random.Next(1, 10000);
+            }
+            return numbers;
+        }
+
+        public static int[] GetTopNumbers(int[] numbers, int count)
+        {
+            count = count / 2;
+            return numbers
+                .OrderByDescending(n => n)
+                .Take(count)
+                .ToArray();
+        }
+
+        public static void PrintArray(int[] array, string text)
+        {
+            foreach (int num in array)
+            {
+                Console.WriteLine(text + num);
+            }
+        }
+
+        public static void ExportToCSV(string filename, double[] tiempos, int cantTest, int[] canNum) 
+        {
+            using (StreamWriter writer = new StreamWriter(filename))
+            {
+                writer.WriteLine("Numero de prueba;Cantidad de numeros;Tiempo (ms)");
+                for (int i = 0; i < cantTest; i++)
+                {
+                    writer.WriteLine($"{i + 1};{canNum[i]};{tiempos[i]:F2}");
+                }
+            }
+        }
+    }
+}
